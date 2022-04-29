e6368eb04c9999afe2c4e376c45e5526b6c9f8b2
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-04-29 20:51:45 +0200

    Link to inscription form added

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,7 @@
     <nav role="navigation">
         <ul>
             <li><a href="index.php?page=home">Accueil</a></li>
+            <li><a href="index.php?page=inscription">Inscrivez-vous !</a></li>
         </ul>
     </nav>
 </header>
