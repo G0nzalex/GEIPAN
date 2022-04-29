2a8ac253d3de95a99d90e98d3873b2a9d0e1d1bf
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-04-29 21:20:28 +0200

    Correction added

--- includes/nav.php
+++ includes/nav.php
@@ -2,7 +2,7 @@
     <nav role="navigation">
         <ul>
             <li><a href="index.php?page=home">Accueil</a></li>
-            <li><a href="index.php?page=inscription">Inscrivez-vous !</a></li>
+            <li><a href="index.php?page=registration">Inscrivez-vous !</a></li>
         </ul>
     </nav>
 </header>
