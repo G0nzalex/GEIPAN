fe0aa3edf610a57501cb600f310ded8e37b99d99
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-05-01 13:34:19 +0200

    The registration form is now complete

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -18,25 +18,49 @@ if (isset($_POST['registration']))
 {
     $registrationTreatment->setName($_POST['name']);
     $name = $registrationTreatment->getName();
-    echo $errMsg = $registrationTreatment->errMsg($name, "There is an issue with the name you typed, please try again");
+    $errMsg = $registrationTreatment->errMsg($name, "<p>There is an issue with the name you typed, please try again</p>");
     $registrationTreatment->errCount($err, $errMsg);
 
     $registrationTreatment->setFirstName($_POST['firstname']);
     $firstname = $registrationTreatment->getFirstName();
-    echo $errMsg = $registrationTreatment->errMsg($firstname, "There is an issue with the first name you typed, please try again");
-
+    $errMsg = $registrationTreatment->errMsg($firstname, "<p>There is an issue with the first name you typed, please try again</p>");
+    $registrationTreatment->errCount($err, $errMsg);
     // $registrationTreatment->setName($_POST['name']);
     // $name = $registrationTreatment->getName();
     // echo $errMsg = $registrationTreatment->errMsg($name, "There is an issue with the name you typed, please try again");
 
     $registrationTreatment->setEmail($_POST['email']);
     $email = $registrationTreatment->getEmail();
-    echo $errMsg = $registrationTreatment->errMsg($email, "There is an issue with the name you typed, please try again");
+    $errMsg = $registrationTreatment->errMsg($email, "<p>There is an issue with the email you typed, please try again</p>");
+    $registrationTreatment->errCount($err, $errMsg);
 
     $registrationTreatment->setPassword($_POST['password']);
     $password = $registrationTreatment->getPassword();
-    echo $errMsg = $registrationTreatment->errMsg($password, "There is an issue with the name you typed, please try again");
+    $errMsg = $registrationTreatment->errMsg($password, "<p>There is an issue with the password you typed, please try again</p>");
+    $registrationTreatment->errCount($err, $errMsg);
 
-    $query->select("SELECT * FROM users WHERE userName= \"$name\"");
-    dumps($name, $firstname, $email, $password);
+    if (count($err) === 0)
+    {
+        $result = $query->select("SELECT * FROM users WHERE userName= \"$name\"");
+        if (count($result) === 0)
+        {
+            $role = 2;
+            $query->insert("INSERT INTO users (userName, userFirstname, userMail, userPassword, id_role)
+            VALUES (\"$name\", \"$firstname\", \"$email\", \"$password\", \"$role\")
+            ");
+            echo $success = "<p>Insertion succesful !</p>";
+        }
+        else
+        {
+            echo $errMsg = "<p>You have been already registered</p>";
+        }
+    }
+    elseif (count($err) === 1)
+    {
+        echo $errMsg;
+    }
+    else
+    {
+        echo $errMsg = "<p>Many inputs have not been correctly filled in, please try again</p>";
+    }
 }
