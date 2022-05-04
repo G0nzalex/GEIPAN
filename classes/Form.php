bff3b734425d93273491a6f8b155fbb93bc3833c
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-05-04 23:03:54 +0200

    Classes and traits have been compacted

--- classes/Form.php
+++ classes/Form.php
@@ -2,6 +2,9 @@
 
 trait Form
 {
+    private string $form;
+    private string $endofForm;
+
     public function form(string $page, ?string $option = null) : string
     {
         return $this->form = "<form action=\"index.php?page=$page\" method=\"post\" $option>";
@@ -10,4 +13,21 @@ trait Form
     {
         return $this->endofForm = "<input type=\"submit\" name=\"$page\" id=\"$page\">" . "</form>";
     }
+    public function display(array $inputs, ?string $br = null) : void
+{
+    for ($i = 0; $i < count($inputs); $i++)
+    {
+        echo $inputs[$i] . $br;
+    }
+}
+    public function errMsg(string $input, string $errMsg) : string
+    {
+        return strlen($input) === 0 ? $errMsg : "";
+    }
+    public function errCount(array &$errors, string $errMsg) : void
+    {
+        if (isset($errors) && strlen($errMsg) !== 0)
+            array_push($errors, "");
+    }
+    // public function
 }
--- classes/Registration.php
+++ classes/Registration.php
@@ -8,7 +8,8 @@ class Registration
     private string $email;
     private string $password;
     private string $pp;
- 
+
+    // Inputs
     public function name() : string
     {
         $this->name = "<label for=\"name\">Your name : </label>";
@@ -44,4 +45,55 @@ class Registration
 
         return $this->password;
     }
+    // Treating data sent by the user
+    public function getName() : string
+    {
+        return !isset($this->name) || strlen($this->name) === 0 || !ctype_alpha($this->name) ? $this->name = "" : mb_strtoupper($this->name);
+
+    }
+    public function setName(string $name) : void
+    {
+        $this->name = $name;
+    }
+    public function getFirstName() : string
+    {
+        return !isset($this->firstName) || strlen($this->firstName) === 0 || !ctype_alpha($this->firstName) ? $this->firstName = "" : $this->firstName;
+    }
+    public function setFirstName(string $firstName) : void
+    {
+        $this->firstName = $firstName;
+    }
+    public function getEmail() : string
+    {
+        return !isset($this->email) || strlen($this->email) === 0 || !filter_var($this->email, FILTER_VALIDATE_EMAIL) ? $this->email = "" : $this->email;
+    }
+    public function setEmail(string $email) : void
+    {
+        $this->email = $email;
+    }
+    public function getPassword() : string
+    {
+        return !isset($this->password) || strlen($this->password) === 0 ? $this->password = "" : $this->password;
+    }
+    public function getPasswordHash() : string
+    {
+        return !isset($this->password) || strlen($this->password) === 0 ? $this->password = "" : password_hash($this->password, PASSWORD_DEFAULT);
+    }
+    public function setPassword(string $password) : void
+    {
+        $this->password = $password;
+    }
+    // public function passwordHash(string $password) : void
+    // {
+    //     $this->password = password_hash($password, PASSWORD_DEFAULT);
+    // }
+    public function getpp() : string
+    {
+        return isset($_FILES['pp']) && $_FILES['pp']['error'] == 0 ? $this->pp : $this->pp = "";
+    }
+    public function setpp(string $path, string $pp) : void
+    {
+        $this->pp = $path . "/" . $pp;
+    }
+    
 }
