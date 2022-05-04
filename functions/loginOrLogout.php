c91465ac493c78a67da89fa2f0e2203d6f5f640a
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-05-04 23:04:43 +0200

    Link added

--- functions/loginOrLogout.php
+++ functions/loginOrLogout.php
@@ -5,6 +5,7 @@ function loginOrlogout()
     if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
         echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
         echo "<li><a href=\"index.php?page=account\">My account</a></li>";
+        echo "<li><a href=\"index.php?page=events\">Add an event</a></li>";
     }
     else {
         echo "<li><a href=\"index.php?page=login\">Login</a></li>";
